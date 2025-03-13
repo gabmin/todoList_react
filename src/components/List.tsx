e9d11378adb743186e8c71ad944be728140c9db4
gabmin <rlarkqals@gmail.com>
2025-03-13 15:37:01 +0900

    [feat] list 컴포넌트 UI 생성

---
+++ src/components/List.tsx
@@ -0,0 +1,15 @@
+import './List.css'
+
+const List = () => {
+
+  return (
+    <div className='list'>
+      <h4>
+        Todo List 🐥
+      </h4>
+      <input placeholder='검색어를 입력하세요'/>
+    </div>
+  )
+}
+
+export default List;
